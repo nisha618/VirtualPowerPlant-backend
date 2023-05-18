e7989340fd8c764e6a0fa9f6fc3965decf65b74a
Nisha Sharma <nishapoudel8427@gmail.com>
2023-05-18 16:22:56 +0545

    first commit

---
+++ src/main/java/com/vpp/vpp/VppApplication.java
@@ -0,0 +1,13 @@
+package com.vpp.vpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vpp/vpp/VppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpp.vpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
